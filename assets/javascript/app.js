81e7a195a9acdc62bbd6fb6c09ec393bd0a38ec4
Monique Robinson <monique.robinson2@gmail.com>
2018-02-24 17:44:26 -0500

    Created Questions and Answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,61 @@
 //Create Variables
-var timer = 0;
+var timer = 30;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
+var intervalId;
 
 //Create Questions/Answers
 
-var questions = {
- firstQuestion: "What breakfast cereal was Sonny the Cuckoo Bird 'cuckoo for'?";
- firstAnswers: ["Fruit Loops", "Captain Crunch", "Cocoa Puffs", "Cinnamon Toast Crunch"];
+var questions = ["What breakfast cereal was Sonny the Cuckoo Bird 'cuckoo for'?", "What is the most popular Super Bowl food?", "What food is Philadephia Known For?", "What is the San Francisco Treat?", "What beverage is like brunch in a cup?"];
 
- secondQuestion:
- secondAnswers:
+var answers = ["Fruit Loops", "Captain Crunch", "Cocoa Puffs", "Cinnamon Toast Crunch"], ["Nachos", "Pizza", "Mozzerella Sticks", "Chicken Wings"], ["Pizza", "Cheesesteak", "Lasagna", "Bell Peppers"], ["Rice-a-roni", "Mac-n-cheese", "Brownies", "Donuts"], ["Milkshake", "Smoothie", "Bloody Mary", "Mimosa"];
 
- thirdQuestion:
- thirdAnswers:
-
- fourthQuestion:
- fourthAnswers:
-
- fifthQuestion:
- fifthAnswers:
+var correctAnswers = ["Cocoa Puffs", "Chicken Wings", "Cheesesteak", "Rice-a-roni", "Bloddy Mary"];
 
 
-
-}
 
 //Set timer
+// function decrement() {
 
+  function run() {
+    //Clear the intervalId first to allow multiple instances
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+
+  function decrement() {
+
+    timer--;
+
+    $("#timer").text("Time Remaining:" + " " + timer);
+
+    if (timer === 0) {
+
+      stop();
+
+      alert("Time Up!");
+    }
+  }
+
+  function stop() {
+
+    clearInterval(intervalId);
+  }
+
+  run();
+
+
+
+  
+
+  // if (timer === 0) {
+
+  //   stop();
+  // }
+
+
+//  = setInterval(decrement, 1000);
 
 //Create event listener for hover
 
