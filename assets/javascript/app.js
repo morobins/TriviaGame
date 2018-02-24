77ab00ed49a6abb0fec1fe81a63a169b9fb974db
Monique Robinson <monique.robinson2@gmail.com>
2018-02-24 15:48:51 -0500

    Added JavaScript and changed HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+//Create Variables
+var timer = 0;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+
+//Create Questions/Answers
+
+var questions = {
+ firstQuestion: "What breakfast cereal was Sonny the Cuckoo Bird 'cuckoo for'?";
+ firstAnswers: ["Fruit Loops", "Captain Crunch", "Cocoa Puffs", "Cinnamon Toast Crunch"];
+
+ secondQuestion:
+ secondAnswers:
+
+ thirdQuestion:
+ thirdAnswers:
+
+ fourthQuestion:
+ fourthAnswers:
+
+ fifthQuestion:
+ fifthAnswers:
+
+
+
+}
+
+//Set timer
+
+
+//Create event listener for hover
+
+//Create event listener for when an answer is clicked
+
+
+
